26bda424878fe5335bca23a33aa3a50b3424cf04
santosh9991 <santu1santosh@gmail.com>
2023-12-09 19:03:34 -0700

    hello world using react

---
+++ App.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client"
+import React from "react"
+
+const App = () => {
+    return <div><h1>Hello React</h1></div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(App)
